5853959ffd0eacce1c1ba34d2ee6bd5f7f5cc658
DM231 <diaa.mostfa2002@gmail.com>
2025-01-15 13:40:27 +0300

    Initial commit

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Repository;
+using BulkyBook.Models;
+using BulkyWeb.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Repository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyBook.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyWeb.DataAccess.Data;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
